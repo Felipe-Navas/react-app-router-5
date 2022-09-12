f028a372931eedb70bdd1d288958b6215db440d0
Felipe Navas <felipenavas.itec@gmail.com>
2022-09-12 09:01:04 -0300

    Trying nested

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,4 +1,5 @@
 import { lazy, LazyExoticComponent } from 'react'
+import { NoLazy } from '../02-lazyload-nested/pages/NoLazy'
 
 type JSXComponent = () => JSX.Element
 
@@ -9,29 +10,22 @@ interface Route {
   children?: Route[]
 }
 
-const LazyPage1 = lazy(() => import('../01-lazyload/pages/LazyPage1'))
-const LazyPage2 = lazy(() => import('../01-lazyload/pages/LazyPage2'))
-const LazyPage3 = lazy(() => import('../01-lazyload/pages/LazyPage3'))
+const LazyLayout = lazy(
+  () =>
+    import(
+      /* webpackChunkName: 'LazyLayout' */ '../02-lazyload-nested/layout/LazyLayout'
+    )
+)
 
 export const routes: Route[] = [
   {
-    path: '/lazy1',
-    Component: LazyPage1,
-    name: 'LazyPage-1',
+    path: '/lazyload',
+    Component: LazyLayout,
+    name: 'LazyLoading Nested',
   },
   {
-    path: '/lazy2',
-    Component: LazyPage2,
-    name: 'LazyPage-2',
-  },
-  {
-    path: '/lazy3',
-    Component: LazyPage3,
-    name: 'LazyPage-3',
-  },
-  {
-    path: '/lazy4',
-    Component: LazyPage3,
-    name: 'LazyPage-4',
+    path: '/no-lazy',
+    Component: NoLazy,
+    name: 'NoLazy Loading',
   },
 ]
